f0c172a233c3d359684992fe58c9810302895f6f
houssamlambara <lambarahoussam@gmail.com>
2024-11-26 15:50:12 +0100

    changement de cart

--- script.js
+++ script.js
@@ -9,3 +9,43 @@
  closeModalButton.addEventListener('click', function() {
      modal.classList.add('hidden');  
  });
+
+ positionPlayer.addEventListener('change',()=>{
+    if(positionPlayer.value=="GK")
+    {
+        statistique.innerHTML=`
+        <label for="" >rating</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >rating</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >rating</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >rating</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >rating</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >rating</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >rating</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        `
+    }else{
+        statistique.innerHTML=
+        `
+         <label for="" >rating</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >pace</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >shooting</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >passing</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >dribbling</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >defending</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        <label for="" >physical</label>
+        <input type="number" class="rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Player's note">
+        `  
+    }
+})
